28d0f3616a4b1d29b3a5e0b3b466eb424a2e84c1
Ye Yint Thu <thuyeyint018@gmail.com>
2024-01-04 12:15:51 +0630

    final commit

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,40 @@
     <h1>How fast can you catch me?</h1>
     <button @click="start" :disabled="isPlaying">Play</button>
     <div v-if="isPlaying">
-        <Block :delay="delay"/>
+        <Block :delay="delay" @endGame="endGame"/>
+    </div>
+    <div v-if="end">
+        <Result :score="score"/>
     </div>
 </template>
 
 <script>
 import Block from "./components/Block.vue";
+import Result from "./components/Result.vue";
 export default {
     name: 'App',
     components: {
-        Block
+        Block,
+        Result
     },
     data(){
         return{
             isPlaying: false,
-            delay: null
+            delay: null,
+            score: 0,
+            end: false
         }
     },
     methods:{
         start(){
             this.isPlaying = true;
+            this.end=false;
             this.delay=2000+Math.random()*5000;
+        },
+        endGame(score){
+            this.score=score;
+            this.isPlaying=false;
+            this.end=true;
         }
     }
 }
--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,12 +1,37 @@
 <template>
-    <div class="block">
-        <h3>Click Here {{delay}}</h3>
+    <div class="block" v-if="showBlock" @click="stopTimer">
+        <h3>Click Here</h3>
     </div>
 </template>
 
 <script>
 export default {
-    props:['delay']
+    props:['delay'],
+    data(){
+        return {
+            showBlock: false,
+            score:0,
+            timer: null
+        }
+    },
+    mounted(){
+        setTimeout(()=>{
+            this.showBlock = true;
+            this.startTimer()
+        },this.delay)
+    },
+    methods:{
+        startTimer(){
+            this.timer=setInterval(()=>{
+                this.score+=50;
+            },50);
+        },
+        stopTimer(){
+            clearInterval(this.timer);
+            this.$emit("endGame",this.score)
+        }
+
+    }
 }
 </script>
 
